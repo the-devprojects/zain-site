bec8ba43e40ffa25f5d98eb861d9ccef57263d76
Zain ul Abideen <digitalzain14a@gmail.com>
2026-02-21 15:29:09 +0500

    Update redirect settings via settings page

--- redirect.js
+++ redirect.js
@@ -4,7 +4,7 @@
 // Ya settings page se bhi change kar sakte hain
 // ============================================
 
-var redirectURL = ""; // <-- Yahan apni link daalein, e.g. "https://google.com"
+var redirectURL = "https://www.effectivegatecpm.com/g385bj33z?key=6db1c60b3931d7cee0ae653a565a3652"; // <-- Yahan apni link daalein, e.g. "https://google.com"
 
 // ============================================
 // REDIRECT LOGIC — Isko change karne ki zaroorat nahi

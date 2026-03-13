ad548be185ec2674865419abf86a577b6889f390
The DevProjects <thedevprojects.official@gmail.com>
2026-03-13 22:17:37 +0500

    Update redirect settings via settings page

--- redirect.js
+++ redirect.js
@@ -4,7 +4,7 @@
 // Ya settings page se bhi change kar sakte hain
 // ============================================
 
-var redirectURL = "https://www.effectivegatecpm.com/p6fepvgn?key=a842ad438de835d9ed92f7fcb038ea88"; // <-- Yahan apni link daalein, e.g. "https://google.com"
+var redirectURL = "https://www.effectivegatecpm.com/y14atyqzh?key=89c5a88c2e8ddf5bba6577937482cff5"; // <-- Yahan apni link daalein, e.g. "https://google.com"
 
 // ============================================
 // REDIRECT LOGIC — Isko change karne ki zaroorat nahi

43c014f189abbceb8f749a877b72851d081489f3
The DevProjects <thedevprojects.official@gmail.com>
2026-03-17 16:47:04 +0500

    Update redirect settings via settings page

--- redirect.js
+++ redirect.js
@@ -4,7 +4,7 @@
 // Ya settings page se bhi change kar sakte hain
 // ============================================
 
-var redirectURL = ""; // <-- Yahan apni link daalein, e.g. "https://google.com"
+var redirectURL = "https://divinelane.org/shuk/index.php?utm_source=Top&utm_medium=Zain"; // <-- Yahan apni link daalein, e.g. "https://google.com"
 
 // ============================================
 // REDIRECT LOGIC — Isko change karne ki zaroorat nahi

609c2247df21847012f2ad101b0e260718b5afa2
The DevProjects <thedevprojects.official@gmail.com>
2026-03-20 14:05:19 +0500

    Update redirect settings via settings page

--- redirect.js
+++ redirect.js
@@ -4,7 +4,7 @@
 // Ya settings page se bhi change kar sakte hain
 // ============================================
 
-var redirectURL = "https://search.link347xx.top/WhDMSKCtF"; // <-- Yahan apni link daalein, e.g. "https://google.com"
+var redirectURL = ""; // <-- Yahan apni link daalein, e.g. "https://google.com"
 
 // ============================================
 // REDIRECT LOGIC — Isko change karne ki zaroorat nahi
